ee4b165736c75d1ae895e0856a1a94468b864a60
Areesha shehzad <157312844+Areesha48@users.noreply.github.com>
2024-05-18 23:14:07 +0500

    Add files via upload

---
+++ script1.js
@@ -0,0 +1,18 @@
+//chap1 //
+//question 1 and 2//
+alert("Welcome to Java");
+
+//question 3//
+alert("welcome to JS land...\nHappy coding!");
+
+//question 4//
+alert("Welcome to javascipt land...")
+alert("Happy coding!\nPrevent this page from creating additional dialogs");
+
+//question 5//
+console.log("Hello... I can run through my web browser's console");
+alert("Hello.. I can run through my web broswer's console");
+
+
+
+
